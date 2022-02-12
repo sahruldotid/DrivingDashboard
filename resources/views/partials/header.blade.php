d8cd134b2b64ce7f053ff8c05ee79f7818f53b27
sahruldotid <sahruldotid@gmail.com>
2022-02-12 14:08:53 +0700

    layouting single html into few files navbar, header and footer

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="https://colorlib.com/polygon/gentelella/images/favicon.ico" type="image/ico" />
+    <title>Gentelella Alela! | </title>
+    <link href="https://colorlib.com/polygon/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://colorlib.com/polygon/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://colorlib.com/polygon/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link href="https://colorlib.com/polygon/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <link href="https://colorlib.com/polygon/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css"
+        rel="stylesheet">
+    <link href="https://colorlib.com/polygon/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+    <link href="https://colorlib.com/polygon/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link href="https://colorlib.com/polygon/build/css/custom.min.css" rel="stylesheet">
+    <meta name="robots" content="index, nofollow">
