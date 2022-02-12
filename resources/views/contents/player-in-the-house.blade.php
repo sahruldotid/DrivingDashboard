693a6061fc7ad9c45931334ca08e2496eca04328
sahruldotid <sahruldotid@gmail.com>
2022-02-12 15:30:23 +0700

    creating player in the house frontend design

---
+++ resources/views/contents/player-in-the-house.blade.php
@@ -0,0 +1,23 @@
+<div class="tile_count">
+    <div class="col-md-3 col-sm-4  tile_stats_count">
+        <span class="count_top"><i class="fa fa-user"></i> Player in The House</span>
+        <div class="count">2500</div>
+        <span class="count_bottom"><i class="green">4% </i> Dari Satu Jam yang Lalu</span>
+    </div>
+    <div class="col-md-3 col-sm-4  tile_stats_count">
+         <span class="count_top"><i class="fa fa-user"></i> Member in The House</span>
+        <div class="count">123.50</div>
+        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> Dari Satu Jam yang Lalu</span>
+    </div>
+    <div class="col-md-3 col-sm-4  tile_stats_count">
+        <span class="count_top"><i class="fa fa-user"></i> Guest Active</span>
+        <div class="count green">2,500</div>
+        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> Dari Satu Jam yang Lalu</span>
+    </div>
+
+    <div class="col-md-3 col-sm-4  tile_stats_count">
+        <span class="count_top"><i class="fa fa-user"></i> Player Closed</span>
+        <div class="count">4,567</div>
+        <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> Dari Satu Jam yang Lalu</span>
+    </div>
+</div>
