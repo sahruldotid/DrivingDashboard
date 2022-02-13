8a235a21cfd5198ab712d539081eb5c5516ead96
sahruldotid <sahruldotid@gmail.com>
2022-02-13 13:01:58 +0700

    fixing player in the house

--- resources/views/contents/player-in-the-house.blade.php
+++ resources/views/contents/player-in-the-house.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-3 col-sm-4  tile_stats_count">
         <span class="count_top"><i class="fa fa-user"></i> Player in The House</span>
         <div class="count">2500</div>
-        <span class="count_bottom"><i class="green">4% </i> Dari Satu Jam yang Lalu</span>
+        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> Dari Satu Jam yang Lalu</span>
     </div>
     <div class="col-md-3 col-sm-4  tile_stats_count">
          <span class="count_top"><i class="fa fa-user"></i> Member in The House</span>
