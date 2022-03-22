bf52af0f5f9255727581ce3a0256a1d48d715267
sahruldotid <sahruldotid@gmail.com>
2022-03-22 17:36:44 +0700

    finalizing total query

--- app/Http/Controllers/Api/OmzetController.php
+++ app/Http/Controllers/Api/OmzetController.php
@@ -1,15 +1,15 @@
 <?php
-
+// need to simplify query
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class OmzetController extends Controller
+
 {
-
+    // omzet daily
     function daily(Request $request){
-
         $this->validate($request, [
             'startDate'  =>  'required|date',
             'endDate'    =>  'required|date|after_or_equal:start_date'
@@ -44,12 +44,31 @@ class OmzetController extends Controller
                         ) as a
                         group by a.tanggal
                         order by a.tanggal asc");
+
+        $total =  DB::select("select a.tanggal, sum(a.amount) as jumlah from (
+                        select date(od.date_ref) as tanggal, sum(coalesce(ol.dpp_orderlist,0)) as amount
+                        from golf_fnb.order_list ol
+                        left join golf_fnb.order_ref od on od.id_ref=ol.id_ref
+                        inner join master_ma.ware wr on wr.wno=ol.wno
+                        inner join master_ma.deppro dp on wr.dept_code = dp.code
+                        left join golf_fnb.registration rg on rg.no_reg = od.no_ref
+                        left join golf_fnb.customers cs on cs.id_customer = rg.id_customer
+                        left join golf_fnb.customer_type ct on ct.id_tcust = cs.id_tcust
+                        where od.trans_status='CLOSE' and coalesce(od.status,'') != 'CANCELED' and coalesce(od.status,'')='' and date(od.date_ref) >= '$request->startDate' and date(od.date_ref) <= '$request->endDate' and dp.code='420' AND (cs.folio_open=true OR cs.folio_open=false) and ct.cust_type IN ('GUEST', 'MEMBER')
+                        group by ol.code_item, date(od.date_ref), ol.name, ol.wno, ol.price, coalesce(ol.unit_code,''), wr.ware_group  order by date(od.date_ref) asc
+                        ) as a
+                        group by a.tanggal
+                        order by a.tanggal asc");
+
         return response()->json([
             'member' => $member,
-            'guest' => $guest
+            'guest' => $guest,
+            'total' => $total,
         ], 200, [], JSON_PRETTY_PRINT);
 
     }
+
+    // omzet bulanan
     function monthly(Request $request){
         $this->validate($request, [
             'startDate'  =>  'required|date',
@@ -89,12 +108,33 @@ class OmzetController extends Controller
 
                             group by a.tanggal
                             order by a.tanggal asc");
+
+
+        $total = DB::select("select a.tanggal, sum(a.amount) as jumlah from (
+                            select date_trunc('month', od.date_ref) as tanggal, sum(coalesce(ol.dpp_orderlist,0)) as amount
+                            from golf_fnb.order_list ol
+                            left join golf_fnb.order_ref od on od.id_ref=ol.id_ref
+                            inner join master_ma.ware wr on wr.wno=ol.wno
+                            inner join master_ma.deppro dp on wr.dept_code = dp.code
+                            left join golf_fnb.registration rg on rg.no_reg = od.no_ref
+                            left join golf_fnb.customers cs on cs.id_customer = rg.id_customer
+                            left join golf_fnb.customer_type ct on ct.id_tcust = cs.id_tcust
+                            where od.trans_status='CLOSE' and coalesce(od.status,'') != 'CANCELED' and coalesce(od.status,'')='' and date(od.date_ref) >= '$request->startDate' and date(od.date_ref) <= '$request->endDate' and dp.code='420' AND (cs.folio_open=true OR cs.folio_open=false) and ct.cust_type IN ('GUEST', 'MEMBER')
+                            group by ol.code_item, tanggal, ol.name, ol.wno, ol.price, coalesce(ol.unit_code,''), wr.ware_group
+                            order by tanggal asc
+                            ) as a
+
+                            group by a.tanggal
+                            order by a.tanggal asc");
         return response()->json([
             'member' => $member,
-            'guest' => $guest
+            'guest' => $guest,
+            'total' => $total,
         ], 200, [], JSON_PRETTY_PRINT);
     }
 
+
+    // omzet bulanan ZT3
     function monthly_zt3(Request $request){
         $this->validate($request, [
             'startDate'  =>  'required|date',
@@ -134,9 +174,27 @@ class OmzetController extends Controller
 
                             group by a.tanggal
                             order by a.tanggal asc");
+
+        $total = DB::select("select a.tanggal, sum(a.amount) as jumlah from (
+                            select date_trunc('month', od.date_ref) as tanggal, sum(coalesce(ol.dpp_orderlist,0)) as amount
+                            from golf_fnb.order_list ol
+                            left join golf_fnb.order_ref od on od.id_ref=ol.id_ref
+                            inner join master_ma.ware wr on wr.wno=ol.wno
+                            inner join master_ma.deppro dp on wr.dept_code = dp.code
+                            left join golf_fnb.registration rg on rg.no_reg = od.no_ref
+                            left join golf_fnb.customers cs on cs.id_customer = rg.id_customer
+                            left join golf_fnb.customer_type ct on ct.id_tcust = cs.id_tcust
+                            where od.trans_status='CLOSE' and coalesce(od.status,'') != 'CANCELED' and coalesce(od.status,'')='' and date(od.date_ref) >= '$request->startDate' and date(od.date_ref) <= '$request->endDate' and dp.code='420' AND (cs.folio_open=true OR cs.folio_open=false) AND ct.cust_type IN ('MEMBER', 'GUEST') AND extract(hour from od.date_ref) IN (16, 20)
+                            group by ol.code_item, tanggal, ol.name, ol.wno, ol.price, coalesce(ol.unit_code,''), wr.ware_group
+                            order by tanggal asc
+                            ) as a
+
+                            group by a.tanggal
+                            order by a.tanggal asc");
         return response()->json([
             'member' => $member,
-            'guest' => $guest
+            'guest' => $guest,
+            'total' => $total,
         ], 200, [], JSON_PRETTY_PRINT);
     }
 }
