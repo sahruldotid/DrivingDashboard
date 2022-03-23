7c5a9fe286fea53833de52a91ddf1ab1135f786a
sahruldotid <sahruldotid@gmail.com>
2022-03-23 08:57:09 +0700

    Create function that return first month until now

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -7,13 +7,26 @@ function getArrBeforeToday() {
     for (var i = 30 - 1; i >= 0; i--) {
         var date = new Date(year, month - 1, day - i);
         // var dateStr = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-        var strWeekday = date.toLocaleString('en-us', { weekday: 'short' }).charAt(0);
+        var strWeekday = date.toLocaleString('en-us', { weekday: 'short' });
         var numDay = date.getDate();
         arr.push([numDay, strWeekday]);
     }
     return arr;
 }
 
+function getFirstMonthUntilNow(){
+    var mo = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+    var arr = [];
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth();
+    for (var i = 0; i <= month; i++) {
+        arr.push([mo[i]]);
+    }
+    return arr;
+
+
+}
 
 var today = new Date();
 document.getElementById('day').innerHTML = today.toLocaleString('en-us', { weekday: 'long' });
--- resources/views/contents/omzet-bulanan-zt3.blade.php
+++ resources/views/contents/omzet-bulanan-zt3.blade.php
@@ -13,7 +13,7 @@
                      const omzet_bulanan_zt3 = new Chart(ctx_zt3, {
                          type: 'line',
                          data: {
-                             labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+                             labels: getFirstMonthUntilNow(),
                              datasets: [{
                                  label: 'Omzet Member',
                                  data: Array(12).fill().map(() => Math.round(Math.random() * 12)),
@@ -35,55 +35,10 @@
                                  borderColor: 'rgb(75, 192, 192)',
                                  tension: 0.1
                              }]
-
                          },
-
                      });
-
                  </script>
              </div>
-             {{-- <div class="col-md-3 col-sm-3  bg-white">
-                 <div class="x_title">
-                     <h2>Top Campaign Performance</h2>
-                     <div class="clearfix"></div>
-                 </div>
-                 <div class="col-md-12 col-sm-12 ">
-                     <div>
-                         <p>Facebook Campaign</p>
-                         <div class="">
-                             <div class="progress progress_sm" style="width: 76%;">
-                                 <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="80"></div>
-                             </div>
-                         </div>
-                     </div>
-                     <div>
-                         <p>Twitter Campaign</p>
-                         <div class="">
-                             <div class="progress progress_sm" style="width: 76%;">
-                                 <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="60"></div>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="col-md-12 col-sm-12 ">
-                     <div>
-                         <p>Conventional Media</p>
-                         <div class="">
-                             <div class="progress progress_sm" style="width: 76%;">
-                                 <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="40"></div>
-                             </div>
-                         </div>
-                     </div>
-                     <div>
-                         <p>Bill boards</p>
-                         <div class="">
-                             <div class="progress progress_sm" style="width: 76%;">
-                                 <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="50"></div>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
-             </div> --}}
              <div class="clearfix"></div>
          </div>
      </div>
