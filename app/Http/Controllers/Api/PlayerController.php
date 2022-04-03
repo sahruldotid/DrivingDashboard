ee2e86610a13f3fd72e97b23d909c0590f83d3c3
woods490 <helloworld7b@gmail.com>
2022-04-03 12:16:39 +0700

    Fixed query on monthly total players

--- app/Http/Controllers/Api/PlayerController.php
+++ app/Http/Controllers/Api/PlayerController.php
@@ -49,27 +49,33 @@ class PlayerController extends Controller{
             'endDate'    =>  'required|date|after_or_equal:start_date'
         ]);
         
-        $member = DB::select("select to_char(a.tanggal, 'YYYY-MM') as tanggal, COUNT(a) as playertot from (
-            select date_trunc('month', ol.log_ts_ins) as tanggal
+        $member = DB::select("select a.tanggal, SUM(a.amount) as playertot from (
+            select date_trunc('month', ol.log_ts_ins) as tanggal, count(coalesce(ol.dpp_orderlist,0)) as amount 
             from golf_fnb.order_list ol
-            where date(ol.log_ts_ins) >= '$request->startDate' and date(ol.log_ts_ins) <= '$request->endDate' and ol.wno='03' and ol.name LIKE '%Member%' 
+	        left join golf_fnb.order_ref od on od.id_ref=ol.id_ref
+            inner join master_ma.ware wr on wr.wno=ol.wno
+            inner join master_ma.deppro dp on wr.dept_code = dp.code
+            where od.trans_status='CLOSE' and coalesce(od.status,'') != 'CANCELED' and coalesce(od.status,'')='' and date(ol.log_ts_ins) >= '2008-01-01' and date(ol.log_ts_ins) <= '2008-02-01' and dp.code='420' and ol.wno='03' and ol.name LIKE '%Member%' 
             group by ol.code_item, tanggal, ol.name, ol.wno, ol.price, coalesce(ol.unit_code,'')
             order by tanggal asc
             ) as a
 
             group by a.tanggal
-            order by a.tanggal asc");
+            order by SUM(a.amount)");
 
-        $guest = DB::select("select to_char(a.tanggal, 'YYYY-MM') as tanggal, COUNT(a) as playertot from (
-            select date_trunc('month', ol.log_ts_ins) as tanggal
+        $guest = DB::select("select a.tanggal, SUM(a.amount) as playertot from (
+            select date_trunc('month', ol.log_ts_ins) as tanggal, count(coalesce(ol.dpp_orderlist,0)) as amount 
             from golf_fnb.order_list ol
-            where date(ol.log_ts_ins) >= '$request->startDate' and date(ol.log_ts_ins) <= '$request->endDate' and ol.wno='03' and ol.name LIKE '%Guest%' 
+	        left join golf_fnb.order_ref od on od.id_ref=ol.id_ref
+            inner join master_ma.ware wr on wr.wno=ol.wno
+            inner join master_ma.deppro dp on wr.dept_code = dp.code
+            where od.trans_status='CLOSE' and coalesce(od.status,'') != 'CANCELED' and coalesce(od.status,'')='' and date(ol.log_ts_ins) >= '2008-01-01' and date(ol.log_ts_ins) <= '2008-02-01' and dp.code='420' and ol.wno='03' and ol.name LIKE '%Guest%' 
             group by ol.code_item, tanggal, ol.name, ol.wno, ol.price, coalesce(ol.unit_code,'')
             order by tanggal asc
             ) as a
 
             group by a.tanggal
-            order by a.tanggal asc");
+            order by SUM(a.amount)");
 
         $period = $this->getMonthPeriod($request->startDate, $request->endDate);
         $member = $this->formatPlayer($period, json_decode(json_encode($member), true));
